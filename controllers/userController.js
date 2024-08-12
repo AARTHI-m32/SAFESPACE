09c22f9ccd59d05076bbea968bdd4094ec7c5f45
AARTHI-m32 <aarthi.m2022cse@sece.ac.in>
2024-08-12 22:04:42 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,6 +59,7 @@ const login = async(req,res) => {
      
        return res.status(200).json({
              token : token,
+             name : user.username,
             message : "Login Successfully"
         })}
         else
